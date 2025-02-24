9bea0519ddac580cb35b37cf4531e1cba0d2f1f0
sam123114 <samuelbou2000@gmail.com>
2025-02-23 23:03:07 -0500

    added test download command

---
+++ commands/dl_audio.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const { pipeline } = require('stream');
+const fetch = require('node-fetch');
+const { promisify } = require('util');
+const { createAudioResource } = require('@discordjs/voice');
+
+const streamPipeline = promisify(pipeline);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('dl_audio')
+        .setDescription("This is a test function used to download audio file"),
+    async execute(interaction) {
+        const url = `https://cdn.communitydragon.org/15.4.1/champion/gnar/champ-select/sounds/choose`;
+        const filePath = './test_audio.ogg'
+
+        try {
+            console.log(`Downloading: ${url}`);
+            const response = await fetch(url);
+
+            if (!response.ok) throw new Error(`Failed to fetch: ${response.statusText}`);
+
+            // Save the file
+            await streamPipeline(response.body, fs.createWriteStream(filePath));
+            console.log('Download complete.');
+        } catch (error) {
+            console.error('Error downloading or playing file:', error);
+            return null;
+        }
+    },
+};
