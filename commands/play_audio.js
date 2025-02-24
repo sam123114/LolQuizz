c4630c3c58bf2252b31a1c39e4e9990f3ccfaea8
sam123114 <samuelbou2000@gmail.com>
2025-02-23 23:57:11 -0500

    test

---
+++ commands/play_audio.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder } = require('discord.js');
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    VoiceConnectionStatus,
+    AudioPlayerStatus
+} = require('@discordjs/voice');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('play_audio')
+        .setDescription("This is a test function used play remote audio file"),
+    async execute(interaction) {
+        const resource = createAudioResource('https://cdn.communitydragon.org/15.4.1/champion/gnar/champ-select/sounds/choose');
+
+        const connection = joinVoiceChannel({
+            channelId: interaction.member.voice.channel.id,
+            guildId: interaction.member.voice.channel.guild.id,
+            adapterCreator: interaction.member.voice.channel.guild.voiceAdapterCreator,
+        });
+
+        const player = createAudioPlayer();
+        connection.subscribe(player);
+
+        connection.on('stateChange', (oldState, newState) => {
+            console.log('Voice connection state changed from', oldState.status, 'to', newState.status);
+
+            if (oldState.status === VoiceConnectionStatus.Ready && newState.status === VoiceConnectionStatus.Connecting) {
+                connection.configureNetworking();
+            }
+        });
+
+        player.on('error', (error) => {
+            console.error('Audio player error:', error);
+        });
+
+        player.on('stateChange', (oldState, newState) => {
+            if (oldState.status !== newState.status) {
+                console.log(`Player state changed from ${oldState.status} to ${newState.status}`);
+            }
+        
+            if (newState.status === AudioPlayerStatus.Idle) {
+                console.log('Audio finished playing!');
+                player.stop();
+                connection.destroy();
+            }
+        });
+
+        if (resource) {
+            player.play(resource);
+            console.log('Playing audio...');
+            interaction.reply({ content: 'Playing audio', ephemeral: true });
+        } else {
+            console.error('Failed to create audio resource.');
+            interaction.reply({ content: 'Failed to create audio resource', ephemeral: true });
+        }
+    },
+};
--- lol_quizz.js
+++ lol_quizz.js
@@ -8,8 +8,6 @@ const {
     entersState,
     VoiceConnectionStatus,
 } = require('@discordjs/voice');
-const https = require('https');
-const fs = require('fs');
 
 class LolQuizz {
 
@@ -297,20 +295,10 @@ class LolQuizz {
         let version = champion_data_helper.getVersion();
 
         if (this.State == "IN_PROGRESS" && connection != null) {
-            // var file = fs.createWriteStream("test_audio.ogg");
-            // var request = https.get("https://cdn.communitydragon.org/${version}/champion/${this.CurrentChampion.id}/champ-select/sounds/choose", function(response) {
-            //     response.pipe(file);
-            // });
-            // console.log(request);
-            const voiceLine = createAudioResource(`/home/ec2-user/projects/LolQuizz/test_audio.ogg`);
-            voiceLine.playStream.on('error', error => {
-                console.error('Error:', error.message, 'with track', resource.metadata.title);
-            });
+            const voiceLine = createAudioResource(`https://cdn.communitydragon.org/${version}/champion/${this.CurrentChampion.id}/champ-select/sounds/choose`);
+
             if (!this.AudioPlayer) {
                 this.AudioPlayer = createAudioPlayer()
-                this.AudioPlayer.on('error', (error) => {
-                    console.error('Audio player error:', error);
-                });
                 connection.subscribe(this.AudioPlayer);
             }
 
